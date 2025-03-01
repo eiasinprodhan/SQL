49815a2894d53e85b5a5e6f3c82dd7e7c233a10c
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-03-01 15:11:54 +0600

    Update Practice.sql

--- Practice.sql
+++ Practice.sql
@@ -9,3 +9,7 @@ WHERE EMPLOYEE_ID NOT IN(100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150);
 SELECT EMPLOYEE_ID "Employee ID", first_name||' '||last_name AS "Full Name", email, phone_number, salary
 FROM employees
 WHERE first_name LIKE '%a_' OR first_name LIKE '_a%';
+
+SELECT first_name, last_name 
+FROM employees
+WHERE SUBSTR(first_name,(LENGTH(first_name)-1), 2) = 'an';
