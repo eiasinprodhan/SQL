738740d4a0b2e3b6ccb24c0bf3738d5fa34d1a8c
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-03-03 16:07:29 +0600

    Update Practicle Exam 03-03-2025.sql

--- Practicle Exam 03-03-2025.sql
+++ Practicle Exam 03-03-2025.sql
@@ -1,7 +1,7 @@
 --QUESTION NO 1
 SELECT first_name, hire_date
 FROM employees
-WHERE TO_CHAR(hire_date,'fmYYYY') BETWEEN '2002' AND '2005';
+WHERE TO_CHAR(hire_date,'fmYYYY') BETWEEN 2002 AND 2005;
 
 --QUESTION NO 2
 SELECT first_name, hire_date
@@ -9,11 +9,15 @@ FROM employees
 WHERE job_id IN ('IT_PROG','SA_MAN');
 
 --QUESTION NO 3
-SELECT first_name ||' ' || last_name AS "Full Name", TO_CHAR(hire_date,'fmDD-MONTH-YYYY')
+SELECT first_name ||' ' || last_name AS "Full Name", hire_date
 FROM employees
-WHERE TO_CHAR(hire_date,'fmDD') >= '01'
-AND TO_CHAR(hire_date,'fmMONTH') >= 'JANUARY'
-AND TO_CHAR(hire_date,'fmYYYY') >= '2008';
+WHERE hire_date > TO_DATE('01-01-2008', 'DD-MM-YYYY');
+
+--OR
+SELECT first_name ||' ' || last_name AS "Full Name", hire_date
+FROM employees
+WHERE hire_date > '01-Jan-2008';
+
 
 --QUESTION NO 4
 SELECT *
