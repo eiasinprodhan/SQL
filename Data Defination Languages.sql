a002d7c8dbe79b0482196ad78df7cca3c046bf1c
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-03-05 16:22:46 +0600

    Changed

--- Data Defination Languages.sql
+++ Data Defination Languages.sql
@@ -17,37 +17,3 @@ INSERT INTO EMP1287470 VALUES(4,'Rahmatullah Khan',70000,'01-Jan-23','Shylet',80
 INSERT INTO EMP1287470 VALUES(5,'Sadiar Rahman',90000,'01-Jan-24','India',850,'JEE');
 
 SELECT * FROM EMP1287470;
-
-
-
-
---Question No 1
-CREATE TABLE emp1287470(
-eid NUMBER(10) CONSTRAINT eid_pk PRIMARY KEY,
-ename VARCHAR2(30) NOT NULL,
-salary NUMBER(10) NOT NULL,
-hire_date DATE NOT NULL,
-address VARCHAR2(30) NOT NULL);
-
---Question No 2
-SELECT department_id, ROUND(AVG(salary),2)
-FROM employees
-GROUP BY department_id
-HAVING MAX(salary)>12000;
-
---Question No 3
-SELECT first_name||' '||last_name AS "Full Name"
-FROM employees
-WHERE first_name LIKE 'S%';
-
---Question No 4
-SELECT AVG(salary)
-FROM employees
-WHERE job_id LIKE '%SA%';
-
---Question No 5
-SELECT first_name||' '||last_name AS "Full Name", hire_date
-FROM employees
-ORDER BY hire_date ASC;
-
-
